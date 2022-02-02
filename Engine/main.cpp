b3fbfc0aed45f3361e3f5efed545d946a820daf0
lawendboy <lawendboy@outlook.com>
2022-02-02 18:21:14 +0100

    Initial Commit

---
+++ Engine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Engine
+//
+//  Created by Michał Walczyk on 02/02/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
