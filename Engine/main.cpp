75b2173618341060a03af0ffdfeed0de7c6dea82
lawendboy <lawendboy@outlook.com>
2022-02-02 21:06:33 +0100

    Dodane biblioteki

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -2,13 +2,11 @@
 //  main.cpp
 //  Engine
 //
-//  Created by Michał Walczyk on 02/02/2022.
-//
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    
 }
